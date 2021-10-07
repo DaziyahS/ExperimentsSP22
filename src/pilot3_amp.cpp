efeb6267e6566c57451d4fc3f6d364eeb2c046cd
Daziyah Sullivan <dhs4@rice.edu>
2021-10-07 15:28:58 -0500

    update to being amp file

---
+++ src/pilot3_amp.cpp
@@ -0,0 +1,28 @@
+/*
+
+Let's do a fake code
+
+// beginning screen
+Subject Number Prompt
+Input Subject Number
+Press submit button
+    store the subject number
+    create an excel file based on the subject number
+
+// transition screen
+update trial number
+make sure the excel file is being created for the following trials
+    create new name for new excel file based on subject number and trial number
+write the first line of the excel file
+tell the person to talk to me, or give them more information
+
+// trial 1
+include information for timestamp
+set up the parameters
+define the base cue parameters
+randomize the parameters wanted
+
+
+
+
+*/
