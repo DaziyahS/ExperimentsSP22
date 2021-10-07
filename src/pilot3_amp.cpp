3eaf4a7da8acf9030bc25bae8dfb882602add0d3
Daziyah Sullivan <dhs4@rice.edu>
2021-10-07 15:44:11 -0500

    finished the pseudo code

--- src/pilot3_amp.cpp
+++ src/pilot3_amp.cpp
@@ -25,7 +25,26 @@ randomize the amplitudes wanted into a vector
     create vector of length 40 with 10 of each amplitude option
     randomize the vector
 display
+    display each of the SAMs with their numbers
+    have a drop down for person to choose which OR have them have buttons underneath them (must hold value chose visibly)
+        updates the valence and arousal values
 press button
+    if not at end of vector
+        record the data
+        reset the values of the valence and arousal display
+        increase the trial iterator
+    else if end of vector && not at max trial
+        record the data
+        go to transition screen
+        reset the trial iterator
+        increase the experiment number
+    else // aka at max trial number
+        record the data
+        go to end of experiment screen
+
+// end of experiment screen
+blank?
+All done. Thank you for your participation
 
 
 
